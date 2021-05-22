9011295cf9daccb8401262cb750d70909a23db72
kuuchan67 <83497138+kuuchan67@users.noreply.github.com>
2021-05-22 16:19:34 +0900

    Initial commit

---
+++ src/router/Router.tsx
@@ -0,0 +1,36 @@
+import { memo, VFC } from "react";
+import { Switch, Route } from "react-router-dom";
+
+import { Login } from "../components/pages/Login";
+import { Page404 } from "../components/pages/Page404";
+
+import { HomeRoutes } from "./HomeRoutes";
+
+export const Router: VFC = memo(() => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Login />
+      </Route>
+      <Route
+        path="/home"
+        render={({ match: { url } }) => (
+          <Switch>
+            {HomeRoutes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      ></Route>
+      <Route path="*">
+        <Page404 />
+      </Route>
+    </Switch>
+  );
+});
