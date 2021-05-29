c3125f4ef2f4f62c07855f27fe7fa17a5049f820
kuuchan67 <83497138+kuuchan67@users.noreply.github.com>
2021-05-29 15:17:08 +0900

    userCard

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,35 +1,17 @@
 import { memo, VFC } from "react";
-import { Box, Image, Text, Stack, Wrap, WrapItem } from "@chakra-ui/react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
+
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
   return (
     <Wrap p={{ base: 4, md: 10 }}>
       <WrapItem>
-        <Box
-          w="260px"
-          h="260px"
-          bg="white"
-          borderRadius="10px"
-          shadow="md"
-          p={4}
-          _hover={{ cursor: "pointer", opacity: 0.8 }}
-        >
-          <Stack textAlign="center">
-            <Image
-              borderRadius="full"
-              boxSize="160px"
-              src="https://source.unsplash.com/random"
-              alt="プロフィール画像"
-              m="auto"
-            />
-            <Text fontSize="lg" fontWeight="bold">
-              名前
-            </Text>
-            <Text fontSize="sm" color="gray">
-              フルネーム
-            </Text>
-          </Stack>
-        </Box>
+        <UserCard
+          imageUrl="https://source.unsplash.com/random/"
+          userName="名前"
+          fullName="フルネーム"
+        />
       </WrapItem>
     </Wrap>
   );
