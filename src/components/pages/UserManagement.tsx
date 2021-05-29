4c0dd3a37fa9a024811fb96fd4d4e46aa6511870
kuuchan67 <83497138+kuuchan67@users.noreply.github.com>
2021-05-29 14:56:28 +0900

    ユーザーカードコンポーネント

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,36 @@
 import { memo, VFC } from "react";
+import { Box, Image, Text, Stack, Wrap, WrapItem } from "@chakra-ui/react";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザ設定</p>;
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <Box
+          w="260px"
+          h="260px"
+          bg="white"
+          borderRadius="10px"
+          shadow="md"
+          p={4}
+          _hover={{ cursor: "pointer", opacity: 0.8 }}
+        >
+          <Stack textAlign="center">
+            <Image
+              borderRadius="full"
+              boxSize="160px"
+              src="https://source.unsplash.com/random"
+              alt="プロフィール画像"
+              m="auto"
+            />
+            <Text fontSize="lg" fontWeight="bold">
+              名前
+            </Text>
+            <Text fontSize="sm" color="gray">
+              フルネーム
+            </Text>
+          </Stack>
+        </Box>
+      </WrapItem>
+    </Wrap>
+  );
 });
